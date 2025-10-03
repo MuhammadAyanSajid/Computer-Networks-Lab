405e47ff007768ea0949ab31bf938e32e96634c7
Muhammad Ayan Sajid <muhammadayansajid2005@gmail.com>
2025-10-03 15:15:21 +0500

    Refactor client.py to implement a loop for continuous message sending and receiving

--- client.py
+++ client.py
@@ -1,11 +1,14 @@
 import socket
 
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
 client_socket.connect(('localhost', 12345))
-msg = input("Enter message: ")
-client_socket.send(msg.encode())
-data = client_socket.recv(1024).decode()
-print( data)
+
+while True:
+    msg = input("Enter message: ")
+    if msg.lower() == "exit":
+        break
+    client_socket.sendall(msg.encode())
+    data = client_socket.recv(1024).decode()
+    print("Received from server:", data)
 
 client_socket.close()
