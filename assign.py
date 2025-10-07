436dc2f4af8bc1ee4caa136d535fa1d9380a5f21
Muhammad Ayan Sajid <muhammadayansajid2005@gmail.com>
2025-10-07 13:27:00 +0500

    Remove redundant comment in assign.py for clarity

--- assign.py
+++ assign.py
@@ -20,7 +20,7 @@ context = ssl.create_default_context()
  
 with smtplib.SMTP(smtp_server, port) as server:
     server.ehlo()  
-    server.starttls(context=context)  # Upgrade connection to secure TLS
+    server.starttls(context=context)  
     server.ehlo()
     server.login(sender_email, password)
     server.sendmail(sender_email, receiver_email, message)
